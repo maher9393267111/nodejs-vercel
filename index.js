c65d496baddf45770ded7e7ef3280ea84cf5c4c7
maher <gomemahero.com>
2022-08-08 02:21:44 +0300

    redeploy

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 
 app.use("/api/product", product);
+app.use("/",  (req, res) => {
+    res.send("Hello World");
+} );
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
